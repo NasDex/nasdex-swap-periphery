b26a102c476c6daed2cbb7ea8f9bb7a6cdbd3692
IwanWang <iwanwang2312@gmail.com>
2021-12-07 19:43:43 +0800

    rename

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,4 @@
 require("@nomiclabs/hardhat-waffle");
-// require("@nomiclabs/hardhat-solhint");
 require("@nomiclabs/hardhat-ethers");
 
 const fs = require('fs');
@@ -53,7 +52,7 @@ module.exports = {
     }
   },
   solidity: {
-    version: "0.5.16",
+    version: "0.6.6",
     settings: {
       optimizer: {
         enabled: true,
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,6 +3,8 @@ const { expect } = require("chai");
 const env = require("hardhat");
 const { ethers } = require("hardhat");
 
+const FACTORY = "0x508B1F76B67381D8BF3aD24E4BFdC7109B03d7a2";
+const WETH = "0x22104669DA1a0a1B3631EdDee6b1B4d41c521F83";
 
 async function main() {
     [deployer] = await ethers.getSigners();
@@ -13,10 +15,10 @@ async function main() {
     
     console.log("Account balance:", (await deployer.getBalance()).toString());
 
-    const NasdexSwapFactory = await ethers.getContractFactory("NasdexSwapFactory");
-    const instance = await NasdexSwapFactory.deploy(deployer.address);
+    const NasdexSwapRouter = await ethers.getContractFactory("NasdexSwapRouter");
+    const instance = await NasdexSwapRouter.deploy(FACTORY, WETH);
 
-    console.log("NasdexSwapFactory address: " + instance.address);
+    console.log("NasdexSwapRouter address: " + instance.address);
 }
 
 main().then(() => process.exit(0)).catch(error => {
